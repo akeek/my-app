51421947bc8077e3f39bdaa9823bfa0ae567f1c6
akebrake <ake.ek@live.com>
2024-11-08 14:44:41 +0100

    setup footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer className="bg-sky-100 mt-auto h-32 text-center">
+            <p className="pt-14">This project was initially built using React as part of a course assignment. Then transitioned from CRA to Next.js, with a shit-ton of help from STI!</p>
+        </footer>
+    )
+}
+
+export default Footer;
